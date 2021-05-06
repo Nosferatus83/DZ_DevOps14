7260ea8693300f7aa2b629282673a2e5cdebe1f1
Кульбида Владимир Александрович <V.Kulbida@omsk.otpbank.ru>
2021-05-06 19:49:25 +0600

    Init

---
+++ simple/deploy.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.66.1"
+    }
+  }
+}
+
+provider "google" {
+  project     = "cosmic-reserve-307720"
+  region      = "europe-west3"
+  zone      = "europe-west3-a"
+}
+
+resource "google_compute_instance" "DZ_DevOps14_Simple" {
+  name         = "DZ-DevOps14"
+  machine_type = "e2-small" // 2vCPU, 2GB RAM
+  #machine_type = "e2-medium" // 2vCPU, 4GB RAM
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      size = "10" 
+      type = "pd-balanced" // pd-standard, pd-balanced, pd-ssd
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP and external static IP
+      #nat_ip = google_compute_address.static.address
+    }
+  }
+
+  metadata = {
+    ssh-keys = "root:${file("/root/.ssh/id_rsa.pub")}" // Point to ssh public key for user root
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install default-jre -y",
+      "sudo apt install nginx -y",
+      "sudo apt install tomcat9 -y"
+    ]
+    connection {
+      type     = "ssh"
+      user     = "root"
+      private_key = file("/root/.ssh/id_rsa")
+      host        = self.network_interface[0].access_config[0].nat_ip
+    }
+  }
+
+  provisioner "file" {
+    source      = "index.html"
+    destination = "/var/www/html/index.html"
+    connection {
+      type     = "ssh"
+      user     = "root"
+      private_key = file("/root/.ssh/id_rsa")
+      host        = self.network_interface[0].access_config[0].nat_ip
+    }
+  }
+  
+  provisioner "file" {
+    source      = "Puzzle15.war"
+    destination = "/var/lib/tomcat9/webapps/Puzzle15.war"
+    connection {
+      type     = "ssh"
+      user     = "root"
+      private_key = file("/root/.ssh/id_rsa")
+      host        = self.network_interface[0].access_config[0].nat_ip
+    }
+  }
+}
---
+++ template/shablon.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.66.1"
+    }
+  }
+}
+
+provider "google" {
+  project     = "cosmic-reserve-307720"
+  region      = "europe-west3"
+  zone      = "europe-west3-a"
+}
+
+
+resource "google_compute_instance" "DZ_Template_VM" {
+  name         = "VM"
+  machine_type = "e2-small" // 2vCPU, 2GB RAM
+  #machine_type = "e2-medium" // 2vCPU, 4GB RAM
+  #machine_type = "custom-6-20480" // 6vCPU, 20GB RAM / 6.5GB RAM per CPU, if needed more refer to next line
+  #machine_type = "custom-2-15360-ext" // 2vCPU, 15GB RAM
+
+  #tags = ["terraform", "template"]
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      size = "10" // size in GB for Disk
+      type = "pd-balanced" // Available options: pd-standard, pd-balanced, pd-ssd
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP and external static IP
+      #nat_ip = google_compute_address.static.address
+    }
+  }
+
+  metadata = {
+    ssh-keys = "root:${file("/root/.ssh/id_rsa.pub")}" // Point to ssh public key for user root
+  }
